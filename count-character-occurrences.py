d3a9148aacf4e905967fa6648535aa44cfb10fd7
Fernando Herrera <faherrera2@gmail.com>
2022-09-02 09:16:31 +0200

    Count character occurences test

--- count-character-occurrences.py
+++ count-character-occurrences.py
@@ -9,3 +9,6 @@ def count_characters(s):
 
 word = input('Enter your string: ')
 count_characters(word)
+
+# administration
+# {'a': 2, 'd': 1, 'm': 1, 'i': 3, 'n': 2, 's': 1, 't': 2, 'r': 1, 'o': 1}
